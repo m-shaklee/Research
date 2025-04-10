ec6691b5ea62e7f7d57801623d7adae02cea2866
m-shaklee <madelineshaklee@gmail.com>
2025-04-10 11:01:46 -0400

    Initial commit

--- STM_cost_no_prolif.py
+++ STM_cost_no_prolif.py
@@ -90,8 +90,8 @@ app.layout = html.Div([
             dcc.Slider(id='dN', min=0, max=0.002, step=0.001, value=0.001,
                        marks={round(i, 4): f"{i:.4f}" for i in np.linspace(0, 0.002, 6)}),
             html.Label("d_STM: Susceptible cell death by STM"),
-            dcc.Slider(id='dSTM', min=0, max=0.02, step=0.001, value=0.001,
-                       marks={round(i, 4): f"{i:.4f}" for i in np.linspace(0, 0.02, 6)}),
+            dcc.Slider(id='dSTM', min=0, max=2, step=0.001, value=0.001,
+                       marks={round(i, 4): f"{i:.4f}" for i in np.linspace(0, 2, 6)}),
         ], style={'width': '48%', 'display': 'inline-block', 'verticalAlign': 'top'})
     ]),
 
